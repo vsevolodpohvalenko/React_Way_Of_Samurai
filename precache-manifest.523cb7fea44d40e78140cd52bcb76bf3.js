10b9f1360bc20211c726a8895cd8de97da62cb31
Seva <vsevolod.pohvalenko@gmail.com>
2020-05-27 19:14:16 +0300

    Updates

---
+++ precache-manifest.523cb7fea44d40e78140cd52bcb76bf3.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d601be34f00c68a34b7733567ad938b",
+    "url": "/Test_React/index.html"
+  },
+  {
+    "revision": "46b262565c568c7704ec",
+    "url": "/Test_React/static/css/3.401cafbc.chunk.css"
+  },
+  {
+    "revision": "d28aab2db9e7fc77030a",
+    "url": "/Test_React/static/css/4.24843fc6.chunk.css"
+  },
+  {
+    "revision": "6599d75180a425368d89",
+    "url": "/Test_React/static/css/main.45a93041.chunk.css"
+  },
+  {
+    "revision": "d583b8d13a66d39e5ce1",
+    "url": "/Test_React/static/js/2.d6e8bcf2.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/Test_React/static/js/2.d6e8bcf2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46b262565c568c7704ec",
+    "url": "/Test_React/static/js/3.29d2a88b.chunk.js"
+  },
+  {
+    "revision": "d28aab2db9e7fc77030a",
+    "url": "/Test_React/static/js/4.c6a4c44c.chunk.js"
+  },
+  {
+    "revision": "6599d75180a425368d89",
+    "url": "/Test_React/static/js/main.0e5c3796.chunk.js"
+  },
+  {
+    "revision": "96c67790cdfb33e9087b",
+    "url": "/Test_React/static/js/runtime-main.ce1e50e1.js"
+  },
+  {
+    "revision": "b997551759008ce663157ee9970e1197",
+    "url": "/Test_React/static/media/ProfilePhoto.b9975517.jpeg"
+  },
+  {
+    "revision": "18dadafdd646309f101f5307740c1f05",
+    "url": "/Test_React/static/media/comment.18dadafd.svg"
+  },
+  {
+    "revision": "87c55bcec393f3019008c7dabc5b91c8",
+    "url": "/Test_React/static/media/profile.87c55bce.jpg"
+  }
+]);
